42ed803494e2b0af7d31d10b7c966e231006d5b3
Michelle-l33 <michelle.lee@spartans.ut.edu>
2024-09-26 16:45:05 -0400

    [FEAT] - added links for routes

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import {Link} from "react-router-dom"
 import "../navstyle.css"
 
 export default function NavBar(){
     return(
         <header>
-            <div class="nav"><a href="Homepage.html">Home</a><a href="AboutMe.html">About Me</a><a href="projects.html">Projects</a><a href="contact.html">Contact</a></div>
+            <div class="nav"><a href="Homepage.html">Home</a><Link to="/aboutme">About Me</Link><a href="projects.html">Projects</a><a href="contact.html">Contact</a></div>
         </header>
     )
 }

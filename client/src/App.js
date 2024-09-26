56c59dd323636e019c08a0aea29dc93b59ca5fbf
Michelle-l33 <michelle.lee@spartans.ut.edu>
2024-09-26 16:24:15 -0400

    [REFACTOR] - moved about me into react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
+import {Route} from "react-router-dom";
 import './App.css';
+import NavBar from '../components/NavBar';
 import ProjectsPage from './components/ProjectsPage';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
-    <ProjectsPage/> //components are thrown in like standalone html tags
+    <div>
+      <NavBar/>
+      <Switch>
+        <Route path = "/aboutme" exact component = {<AboutMe/>}></Route>
+        <Route path = "/projects" exact component = {<ProjectsPage/>}></Route>
+      </Switch>
+      <ProjectsPage/> //components are thrown in like standalone html tags
+    </div>
+    
   );
 }
 
---
+++ client/src/components/AboutMe.jsx
@@ -0,0 +1,62 @@
+import React from "react"
+import NavBar from "./NavBar"
+import "../../src/styleAbout.css"
+
+export default function AboutMe(){
+    return (
+        <>
+            <NavBar/>
+
+            <section class = "title">
+                <h1>About Me</h1>
+            </section>
+
+            <section class = "main">
+                <section class = "maincol">
+                    <img src = "../../publicimages/profilepic.jpg" alt = "Profile picture of person." width ="100%" height="80%"/>
+                    <a href = "https://www.google.com"><button class = "button"><h3>Download Resume</h3> </button></a>
+                </section>
+
+                <section class = "maincol">
+                    <p> I'm ---, a computer science student at University of Tampa. Growing up in a small town in Iowa, I was always fascinated by technology. My curiosity led me to pursue a major in Computer Science, where I quickly developed a passion for programming and algorithms.</p>
+                    <p>After my sophomore year, I interned at a tech startup, working on a smart home mobile app. This hands-on experience taught me the importance of clean, efficient code, and teamwork. My junior year brought a research assistant position focusing on AI and machine learning, where I helped develop a real-time chatbot. This sparked my interest in AI, leading me to take more specialized courses.</p>
+                    <p>Outside of academics, I'm an avid gamer and enjoy building custom gaming PCs. I also contribute to open-source projects on GitHub. In my free time, I love playing with my dog, cooking, and hiking.</p>
+                </section>
+            
+                <section class = "maincol">
+                    <section class = "img-grid">
+                        <section class = "column">
+                            <img src = "../../public/images/utlogo.png" alt = "University of Tampa Logo"/>
+                            <img src = "../../public/images/hiking.jpg" alt = "Man hiking on mountain with snow."/>
+                            <img src = "../../publicimages/dog.jpg" alt = "German shepherd "/>
+                        </section>
+                        <section class = "column">
+                            <img src = "../../publicimages/tech.jpg" alt="Glowing phone with smart home icons."/>
+                            <img src = "../../publicimages/cooking.jpg" alt="Photo with a variety of veggies spread on table with cast iron skillet in the center, as well as other cooking utensils."/>
+                            <img src = "../../publicimages/gaming.jpg" alt="Computer and console gaming essentials. Keyboard, mouse,controller, nintendo switch, etc."/>
+                        </section>
+        
+                    </section>
+                </section>
+            </section>
+            <section class = "title">
+                <h2>Skills</h2>
+            </section>
+            <section class = "bars">
+                <label for="html">HTML Proficiency</label>
+                <progress id="html" value="75" max="100">75%</progress>
+                <label for="html">Python Proficiency</label>
+                <progress id="html" value="95" max="100">95%</progress>
+            </section>
+            <section class = "bars">
+                <label for="html">CSS Proficiency </label>
+                <progress id="html" value="60" max="100">60%</progress>
+                <label for="html">Javascript Proficiency</label>
+                <progress id="html" value="50" max="100">50%</progress>
+            </section>
+            <section class ="totop">
+                <a><button class = "button" id = "topbutton"><h3>Back to Top</h3></button></a>
+            </section>
+        </>
+    )
+};
